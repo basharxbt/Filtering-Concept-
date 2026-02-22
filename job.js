3b010b7e4884b2a81a28882dbdbcda4f0dea979e
Shafiullah Bashar <shafiullahbashaar@gmail.com>
2026-02-23 02:58:33 +0600

    Filter Option Added

--- job.js
+++ job.js
@@ -1,12 +1,22 @@
 const jobContainer = document.getElementById('job-container');
-const totalJobs = jobContainer.childElementCount;
-
-const totalJobsElement = document.getElementById('total-jobs');
-totalJobsElement.innerText = totalJobs;
+const interview = document.getElementById('interview-jobs');
+const rejected = document.getElementById('rejected-jobs');
+const totalJobs = document.getElementById('total-jobs')
+totalJobs.innerText = jobContainer.children.length;
 
 
 let interviewJobsElement = [];
 let rejectedJobsElement = [];
+
+function countCalculator() {
+    interview.innerText = interviewJobsElement.length;
+    rejected.innerText = rejectedJobsElement.length;
+}
+// const totalJobsElement = document.getElementById('total-jobs');
+// totalJobsElement.innerText = totalJobs;
+
+
+
 
 
 const mainContainer = document.querySelector('main');
@@ -22,29 +32,21 @@ function clickToggle(id) {
     interview.classList.remove("bg-neutral-700", "text-white")
     rejected.classList.remove('bg-neutral-700', "text-white")
 
-
     document.getElementById(id).classList.add("bg-neutral-700", "text-white");
 
+    if (id === "interview-btn") {
+        filterSection.classList.remove("hidden");
+        jobContainer.classList.add("hidden");
 
-
-    // if (id === 'all-btn') {
-    //     filterSection.classList.add('hidden');
-
-    //     jobContainer.classList.remove('hidden');
-    // }
-    // else if (id === 'interview-btn') {
-    //     interview.classList.add("bg-neutral-700", "text-white")
-    //     all.classList.remove("bg-neutral-700", "text-white");
-    //     rejected.classList.remove('bg-neutral-700', "text-white")
-    //     filterSection.classList.remove('hidden');
-    //     jobContainer.classList.add('hidden');
-    // }
-
-    // else if (id === 'rejected-btn') {
-    //     rejected.classList.add('bg-neutral-700', "text-white")
-    //     interview.classList.remove("bg-neutral-700", "text-white")
-    //     all.classList.remove("bg-neutral-700", "text-white");
-    // }
+    }
+    else if (id === "all-btn") {
+        filterSection.classList.add("hidden");
+        jobContainer.classList.remove("hidden");
+    }
+    else if (id === "rejected-btn") {
+        filterSection.classList.add("hidden");
+        jobContainer.classList.add("hidden");
+    }
 }
 
 
@@ -76,6 +78,39 @@ mainContainer.addEventListener('click', function (event) {
 
         }
         renderInterviewJobsElement()
+        countCalculator()
+
+
+    }
+    else if (event.target.classList.contains('rejectedButton')) {
+        const parentNode = event.target.parentNode.parentNode;
+
+        const companyName = parentNode.querySelector('.companyName').innerText;
+        const jobTitle = parentNode.querySelector('.jobTittle').innerText;
+        const jobCategory = parentNode.querySelector('.jobCatagory').innerText;
+        const prograss = parentNode.querySelector('.prograss').innerText;
+        const description = parentNode.querySelector('.description').innerText;
+        parentNode.querySelector('.prograss').innerText = 'INTERVIEW';
+
+
+        const cardInfo = {
+            companyName,
+            jobTitle,
+            jobCategory,
+            prograss,
+            description
+        };
+
+        const jobExist = interviewJobsElement.find(job => job.companyName === companyName && job.jobTitle === jobTitle);
+        if (!jobExist) {
+
+            interviewJobsElement.push(cardInfo);
+            console.log(interviewJobsElement);
+
+        }
+        renderInterviewJobsElement()
+        countCalculator()
+
 
     }
 
@@ -94,25 +129,17 @@ function renderInterviewJobsElement() {
     for (let job of interviewJobsElement) {
         let div = document.createElement('div');
 
-        div.className = 'bg-white p-5 rounded-lg shadow-sm flex justify-between'
         div.innerHTML = `
         
             <div class="bg-white p-5 rounded-lg shadow-sm flex justify-between">
                 <div class="">
-                    <p class="companyName font-bold text-[#002C5C] text-[18px] mb-1">Mobile First Corp</p>
+                    <p class="companyName font-bold text-[#002C5C] text-[18px] mb-1">${job.companyName}</p>
 
-                    <p class="jobTittle mb-4 text-neutral-500">React Native Developer</p>
-                    <p class="jobCatagory mb-3 text-neutral-500">Remote
-                        •
-                        Full-time
-                        •
-                        $130,000 - $175,000</p>
-                    <button class="prograss btn btn-soft btn-info">NOT APPLIED</button>
+                    <p class="jobTittle mb-4 text-neutral-500">${job.jobTitle}</p>
+                    <p class="jobCatagory mb-3 text-neutral-500">${job.jobCategory}</p>
+                    <button class="prograss btn btn-soft btn-info">INTERVIEW</button>
 
-                    <p class="description my-3">Build cross-platform mobile applications using React Native. Work on
-                        products used
-                        by millions of
-                        users worldwide.</p>
+                    <p class="description my-3">${job.description}</p>
                     <button class="btn btn-outline btn-success mx-2">INTERVIEW</button>
 
                     <button class="btn btn-outline btn-error">REJECTED</button>
